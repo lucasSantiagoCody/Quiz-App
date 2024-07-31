ee3981f88df1f06e45d40468b29acdfc46a13853
lucas santiago cody <lucassantiagocody@gmail.com>
2024-07-31 12:33:13 +0100

    Create HomePage screeen

---
+++ lib/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.deepPurple[100],
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: Colors.deepPurple.shade400, width: 4),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.deepPurple.withOpacity(0.5),
+                        spreadRadius: 10, // Raio de propagação
+                        blurRadius: 0, // Raio de desfoque
+                        offset: const Offset(0, 10), // Deslocamento da sombra
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: Image.asset('assets/images/logo.jpg'),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('click');
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith((states) {
+                        if (states.contains(MaterialState.hovered)) {
+                          return Colors.deepPurple.shade200;
+                        }
+                        return Colors.deepPurple[400];
+                      }),
+
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+                      mouseCursor:
+                          MaterialStateProperty.all(SystemMouseCursors.click),
+                      
+                      elevation: MaterialStateProperty.resolveWith((states) {
+                        if (states.contains(MaterialState.hovered)) {
+                        return 10; 
+                      }
+                      return 2; 
+                      }),
+                      
+                      ),
+                  child: const Text(
+                    'Play',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}

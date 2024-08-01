7b9e04854d0f5236b06a0667fe2c7c1e6caadea9
lucas santiago cody <lucassantiagocody@gmail.com>
2024-08-01 21:31:42 +0100

    Create MyApp StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
-
+import 'quiz.dart';
+import 'game_over.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( );
+  }
 }

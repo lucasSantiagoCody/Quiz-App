343b642f5abd1e703f3b14f0add7b5a00f3cedc1
lucas santiago cody <lucassantiagocody@gmail.com>
2024-08-01 22:27:21 +0100

    Add all questions in allQuestions List in questions.dart file

---
+++ lib/questions.dart
@@ -0,0 +1,54 @@
+List allQuestions = [
+  {
+    "pergunta": "Qual linguagem de programação é amplamente utilizada para desenvolvimento backend?",
+    "alternativas": ["PHP", "Ruby", "Python"],
+    "alternativa_correcta": 3
+  },
+  {
+    "pergunta": "O que é um banco de dados relacional?",
+    "alternativas": ["Um banco de dados que não possui tabelas", "Um banco de dados que armazena dados em formato JSON", "Um banco de dados que organiza os dados em tabelas "],
+    "alternativa_correcta": 3
+  },
+  {
+    "pergunta": "O que é uma API RESTful",
+    "alternativas": ["Um tipo de linguagem de programação", "Um protocolo de comunicação entre sistemas", "Um sistema de gerenciamento de bancos de dados"],
+    "alternativa_correcta": 2
+  },
+  {
+    "pergunta": "Qual é a função principal de um servidor web no contexto do desenvolvimento backend?",
+    "alternativas": ["Armazenar imagens e vídeos", " Processar requisições HTTP", "Estilizar a interface do usuário"],
+    "alternativa_correcta": 2
+  },
+  {
+    "pergunta": "O que significa CRUD em desenvolvimento web?",
+    "alternativas": ["Criar, Recuperar, Utilizar, Deletar", "Criar, Requisitar, Usar, Deletar", " Criar, Ler, Atualizar, Deletar"],
+    "alternativa_correcta": 3
+  },
+  {
+    "pergunta": "Qual é a diferença entre autenticação e autorização em um sistema web?",
+    "alternativas": ["Autorização verifica se o usuário tem permissão para acessar recursos específicos, enquanto autenticação verifica a identidade do usuário.","Autenticação verifica se o usuário tem permissão para acessar recursos específicos, enquanto autorização verifica a identidade do usuário.",  "Autenticação e autorização são termos intercambiáveis no contexto da programação web."],
+    "alternativa_correcta": 1
+  },
+  {
+    "pergunta": "O que é uma migração de banco de dados em um contexto de desenvolvimento backend?",
+    "alternativas": ["O processo de transferir dados entre servidores diferentes", "O processo de atualizar o software do banco de dados para a versão mais recente", "O processo de modificar a estrutura do banco de dados sem perder os dados existentes"],
+    "alternativa_correcta": 3
+  },
+  {
+    "pergunta": "Qual é a diferença entre uma aplicação web cliente-servidor e uma aplicação web de servidor único?",
+    "alternativas": ["Uma aplicação web cliente-servidor possui apenas um servidor, enquanto uma aplicação web de servidor único possui vários servidores", "Em uma aplicação web cliente-servidor, o processamento é distribuído entre o cliente e o servidor, enquanto em uma aplicação web de servidor único, todo o processamento é feito no servidor", "Não há diferença entre os dois tipos de aplicações web."],
+    "alternativa_correcta": 2
+  },
+  {
+    "pergunta": "O que é um framework no contexto do desenvolvimento backend?",
+    "alternativas": ["Um ambiente de desenvolvimento integrado", "Uma coleção de bibliotecas e ferramentas que ajudam a acelerar o desenvolvimento de aplicações", "Um tipo específico de linguagem de programação"],
+    "alternativa_correcta": 2
+  },
+  {
+    "pergunta": "Qual é a importância da segurança no desenvolvimento backend?",
+    "alternativas": ["A segurança garante que os dados dos usuários sejam protegidos contra acessos não autorizados","A segurança não é importante no desenvolvimento backend",  "A segurança só é importante no desenvolvimento frontend"],
+    "alternativa_correcta": 1
+  },
+
+
+];
